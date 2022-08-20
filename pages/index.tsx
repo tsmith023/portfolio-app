ab77e8aad9c94e40c74d6d6ce592ef448e17252f
Tommy Smith <tommy.smith023@gmail.com>
2022-08-20 23:01:40 +0100

    add HeadlessUI to fade landing poem in and out

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,90 @@
-import React, { useEffect, useRef, useState } from "react";
-import nextPackage from "next/package.json";
-import Title from '../components/Title'
+import React, { useEffect, useState } from "react";
+import { Transition } from '@headlessui/react'
 
 interface StackedCardProps {
-  disabled: boolean,
-  onClick: React.MouseEventHandler<HTMLButtonElement>,
-  stack: React.ReactElement,
+  visible: boolean,
+  // onClick: React.MouseEventHandler<HTMLDivElement>,
+  text: string
 }
 
 const StackedCard: React.FC<StackedCardProps> = (props: StackedCardProps): React.ReactElement => {
+  const onClick = () => {
+    window.open("https://glaemscrafu.jrrvf.com")
+  }
   return (
-    <button
-      className="text-center btn btn-primary border border-quaternary card w-80 bg-primary place-content-center"
-      onClick={props.onClick}
+    <div
+      className={`
+        card w-192 h-20 bg-primary py-10
+        cursor-pointer btn btn-primary
+        text-center text-2xl
+      `}
+      onClick={onClick}
     >
-      <div className="card-body">{ props.stack }</div>
-    </button>
+      <div className={`
+        card-body normal-case
+      `}>{ props.text }</div>
+    </div>
   )
 }
 
-const defaultStacks = [
-  <div>I bloody love physics</div>,
-  <div>I also bloody love coding</div>,
-  <div>And DnB m8</div>
-]
-
 export default function Home(): React.ReactElement {
-  const [ whichStack, setWhichStack ] = useState(0)
-  const [ stacks, setStacks ] = useState(defaultStacks)
+  const texts = [
+    "The desire of my spirit urges me to journey forth",
+    "over the flowing sea, that far hence",
+    "across the hills of water and the whale's country",
+    "I may seek the land of strangers"
+  ]
 
-  const onClick = ( idx: number ) => {
-    const newStackNumber = whichStack + 1 === stacks.length ? 0 : whichStack + 1
-    const poppedStacks = defaultStacks.filter((_, idx) => idx !== newStackNumber)
-    setWhichStack(newStackNumber)
-    setStacks([
-      defaultStacks[newStackNumber],
-      ...poppedStacks
-    ])
+  const [ isShowing, setIsShowing ] = useState(true)
+  const [ whichStack, setWhichStack ] = useState(0)
+  // const [ selectedStack, setSelectedStack ] = useState(defaultStacks[0])
+  // const [ stacks, setStacks ] = useState(defaultStacks)
+
+  const fadeTime = 200
+
+  const onFadeOut = () => {
+    setIsShowing(false)
   }
 
+  const onFadeIn = () => {
+    setIsShowing(true)
+  }
+
+  const onChange = () => {
+    // Begin fade out transition
+    setIsShowing(false)
+    // Calculate new stack to choose from
+    const newStackNumber = whichStack + 1 === texts.length ? 0 : whichStack + 1
+    // Trigger function to execute after fadeTime to change stack and fade back in
+    setTimeout(() => {
+      setWhichStack(newStackNumber)
+      setIsShowing(true)
+    }, fadeTime)
+  }
+
+  useEffect(() => {
+    // Setup time between Stack Fades to trigger on each fade-out-and-in
+    const waitTime = 6000
+    const change = setInterval(() => onChange(), waitTime)
+    return () => clearInterval(change)
+  }, [onChange])
+  
   return (
     <div className="h-full flex">
-      <div className="stack m-auto">
-        { stacks.map((stack, idx) => {
-          return (
-            <StackedCard
-              key={idx}
-              disabled={idx!==whichStack}
-              onClick={() => onClick(idx)}
-              stack={stack}
-            />
-          )
-        }) }
+      <div className="m-auto">
+        <Transition
+          show={isShowing}
+          unmount={false}
+          enter={`transition-opacity duration-${fadeTime}`}
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave={`transition-opacity duration-${fadeTime}`}
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <StackedCard text={texts[whichStack]} visible={isShowing} />
+        </Transition>
       </div>
     </div>
-  );
+  )
 }
